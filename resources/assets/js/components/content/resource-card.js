fbc907b09f318e4ffc038847d672429f0b06e951
jeffrey lee <jeffreylee@kingyoorekis-MacBook-Pro.local>
2018-03-05 16:39:09 -0800

    fixed ga actions

--- resources/assets/js/components/content/resource-card.js
+++ resources/assets/js/components/content/resource-card.js
@@ -79,7 +79,7 @@ class ResourceCard extends Component {
 
         if(this.props.analytics !== undefined) {
             ReactGA.event({
-                category: 'Resource',
+                category: 'Click Through',
                 action: 'Clicked',
             });
         }
